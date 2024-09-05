037980662d3634efb8f4f22333c935057c66ea9b
Andre K. <Andre.kukk@tktk.ee>
2024-09-05 18:39:35 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a commemt to my code
+
+//rakendus küsib kasutajal sisestada tema nimi 
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+String userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" +", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} ! "); //Output
